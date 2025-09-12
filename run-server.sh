b07881ebd77a773ec9e7cfb458856cc541d50440
Abner Serafim Arede <abner.adekz@adekz.com>
2025-09-12 13:41:32 -0300

    Ajustes para prod e server

--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,9 @@
+echo "Stopping and removing any existing containers..."
+echo "docker compose -f ./docker-compose-server.yml down"
+docker compose -f ./docker-compose-server.yml down
+echo "Updating codebase..."
+echo "git pull"
+git pull
+echo "Starting server containers..."
 echo "docker compose -f ./docker-compose-server.yml up -d"
 docker compose -f ./docker-compose-server.yml up -d
